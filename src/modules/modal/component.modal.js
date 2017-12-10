4c4e8212c08e466c996558b29a2e82d8eec2fee1
Evan Short <evan.short@gmail.com>
2017-12-09 19:14:50 -0500

    adding component modal for escape key press

---
+++ src/modules/modal/component.modal.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import SubNavigation from '../subnavigation/component.subnavigation';
+import Panel from '../panel/component.panel';
+import Route from '../route/component.route';
+import Link from '../link/component.link';
+
+class Modal extends React.Component {
+  constructor(){
+    super();
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+
+  render(){
+    return <Panel { ...this.props }
+				 panelName="modal"
+				 navigation={ this.props.modal }
+				 ignoreWhen={ this.props.modal.getIn(["state", "hidden"]) } >
+        <Route when="/modal/decline" exact >
+					<Link
+						 navigation={this.props.modal}
+						 navigateWith={this.props.closeModal}>
+
+							Close
+					</Link>
+					no thanks
+        </Route>
+        <Route when="/modal/accept" exact >
+					sounds good
+						<Link
+							 navigation={this.props.modal}
+							 navigateWith={this.props.closeModal}>
+
+								Close
+						</Link>
+        </Route>
+      </Panel>
+  }
+
+  hideMenu(e){
+    e.preventDefault();
+    this.props.hideMenu();
+  }
+
+  handleKeyPress(e){
+    if(e.keyCode === 27){
+      if(!this.props.modal.getIn(["state", "hidden"])) this.props.closeModal();
+    }else{
+      return true
+    }
+  }
+
+  componentDidMount(){
+    document.addEventListener('keydown', this.handleKeyPress);
+  }
+
+  componentWillUnmount(){
+    document.removeEventListener('keydown', this.handleKeyPress);
+  }
+}
+
+export default Modal
+
