dfd57f5d7259f7ff0e47d952871e811932a714e5
Evan Short <evan.short@gmail.com>
2017-12-09 23:59:47 -0500

    adding base component files

---
+++ src/modules/layout/component.layout.js
@@ -0,0 +1,5 @@
+export { default as Footer } from './footer/component.footer';
+export { default as Header } from './header/component.header';
+export { default as Navigation } from './navigation/component.navigation';
+export { default as SubNavigation } from './subnavigation/component.subnavigation';
+
